be8224def5b39670f9dc61ddd47450f2a5807501
Jan Buschtöns <jan@buschtoens.me>
2019-06-15 11:17:50 +0200

    test: task with modifiers

--- tests/unit/task-test.ts
+++ tests/unit/task-test.ts
@@ -1,5 +1,5 @@
 import { task, timeout } from 'ember-concurrency';
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { get } from '@ember/object';
 
 module('Unit | task', function() {
@@ -13,6 +13,32 @@ module('Unit | task', function() {
         yield timeout(delay);
         return returnValue;
       });
+
+      async perform() {
+        const returnValue: 'foo' = await get(this, 'someTask').perform(
+          1,
+          'foo'
+        );
+        assert.strictEqual(returnValue, 'foo');
+      }
+    }
+
+    const subject = new Subject();
+    await subject.perform();
+  });
+
+  skip('task with modifiers', async function(assert) {
+    class Subject {
+      someTask = task(function*<T>(
+        this: Subject,
+        delay: number,
+        returnValue: T
+      ) {
+        yield timeout(delay);
+        return returnValue;
+      });
+      // .restartable()
+      // .maxConcurrency(1);
 
       async perform() {
         const returnValue: 'foo' = await get(this, 'someTask').perform(
